7d81193e3a5ce5d48769eea6f011bdf26716b734
athina <athinampitou@hotmail.com>
2024-01-27 01:16:23 +0200

    kati ekana

--- project/front/src/app/main-container/keeper-bookings/keeper-bookings.component.ts
+++ project/front/src/app/main-container/keeper-bookings/keeper-bookings.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
 import { AuthService } from 'src/app/services/auth.service';
+import { UserServiceService } from 'src/app/services/user-service.service';
+import { ActivatedRoute } from '@angular/router';
+import { DatePipe } from '@angular/common';
+import { User } from 'src/app/models/user.model';
+import { Booking } from 'src/app/models/booking.model';
+
 
 @Component({
   selector: 'app-keeper-bookings',
@@ -8,10 +14,31 @@ import { AuthService } from 'src/app/services/auth.service';
 })
 export class KeeperBookingsComponent {
 
-  constructor(private authService : AuthService) {}
+  keeper = new User();
+  booking = new Booking();
+  owner = new User();
+  pet = new User();
+
+  constructor(private authService : AuthService, private userService : UserServiceService,private route: ActivatedRoute, private datePipe: DatePipe) {}
+
 
   ngOnInit(): void {
+    this.route.paramMap.subscribe(params => {
+      const userId = params.get('user_id') ; // convert id to a number
+        this.userService.getKeeper(parseInt(userId || '0', 10)).subscribe(
+          data => {
+            this.keeper = data; // Assign the emitted value to user
+            console.log(data);
+          },
+          error => {
+            console.error('Error fetching owner data', error);
+          }
+        );
+    });
+
   }
+
+
 
   onLogout() {
     this.authService.logout();
