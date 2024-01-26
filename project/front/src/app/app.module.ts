d173d50913a8f2b12bc35076c8a02df009831951
athina <athinampitou@hotmail.com>
2024-01-26 23:06:01 +0200

    two home pages

--- project/front/src/app/app.module.ts
+++ project/front/src/app/app.module.ts
@@ -23,6 +23,8 @@ import { AdminHomeComponent } from './main-container/admin-home/admin-home.compo
 import { AdminStatsComponent } from './main-container/admin-stats/admin-stats.component';
 import { KeeperBookingsComponent } from './main-container/keeper-bookings/keeper-bookings.component';
 import { VisitorFindKeepersComponent } from './main-container/visitor-find-keepers/visitor-find-keepers.component';
+import { KeeperHomeComponent } from './main-container/keeper-home/keeper-home.component';
+import { OwnerHomeComponent } from './main-container/owner-home/owner-home.component';
 import { TableModule } from 'primeng/table';
 import { ChartModule } from 'primeng/chart';
 import { Chart } from 'chart.js';
@@ -42,6 +44,8 @@ import { Chart } from 'chart.js';
     AdminStatsComponent,
     KeeperBookingsComponent,
     VisitorFindKeepersComponent,
+    KeeperHomeComponent,
+    OwnerHomeComponent,
   ],
   imports: [
     BrowserModule,
