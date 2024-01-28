a12524f427a5fbe09e724b50af660ef7ceabb5a4
Maou <giorgis2000@gmail.com>
2024-01-28 19:23:20 +0200

    name keeper

--- project/front/src/app/main-container/keepers-review/keepers-review.component.ts
+++ project/front/src/app/main-container/keepers-review/keepers-review.component.ts
@@ -30,6 +30,16 @@ export class KeepersReviewComponent {
 
   ngOnInit(): void {
     this.route.paramMap.subscribe(params => {
+      this.userService.getOwners().subscribe(
+        data => {
+          this.owners = data; // Assign the emitted value to user
+        },
+        error => {
+          console.error('Error fetching owner data', error);
+        }
+      );
+
+
       const userId = params.get('user_id') ; // convert id to a number
 
       this.userService.GetReviewsByKeeper(parseInt(userId || '0', 10)).subscribe(
@@ -44,7 +54,6 @@ export class KeepersReviewComponent {
       this.userService.GetPetKeepersDays(parseInt(userId || '0', 10)).subscribe(
         data => {
           this.days = data; // Assign the emitted value to user
-          console.log(data);
         },
         error => {
           console.error('Error fetching keeper data', error);
@@ -54,7 +63,6 @@ export class KeepersReviewComponent {
       this.userService.GetPetKeepersBookings(parseInt(userId || '0', 10)).subscribe(
         data => {
           this.bookings = data; // Assign the emitted value to user
-          console.log(data);
         },
         error => {
           console.error('Error fetching keeper data', error);
@@ -62,20 +70,10 @@ export class KeepersReviewComponent {
       );
     });
     
-    this.userService.getOwners().subscribe(
-        data => {
-          this.owners = data; // Assign the emitted value to user
-        },
-        error => {
-          console.error('Error fetching owner data', error);
-        }
-      );
   }
 
 
   getOwnerName(id: number) {
-    var mama = this.owners.find(o => o.id === id)?.first_name;
-    console.log(mama);
-    return mama;
+    return this.owners.find(owner => owner.id === id)?.first_name;
   }
 }
